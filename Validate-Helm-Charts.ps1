ece48bbf857aaf602d1f48ebd019b324bc45ebb8
Petro Kolosov <kolosovp94@gmail.com>
2026-04-19 17:51:14 +0200

    update helm charts

--- Validate-Helm-Charts.ps1
+++ Validate-Helm-Charts.ps1
@@ -6,6 +6,6 @@ Set-Location $PSScriptRoot
 
 helm template auth-app .\charts\auth\ --values .\charts\auth\values.yaml --debug
 helm template consumer-app .\charts\consumer\ --values .\charts\consumer\values.yaml --debug
-helm template sender-app .\charts\sender\ --values .\charts\consumer\values.yaml --debug
+helm template sender-app .\charts\sender\ --values .\charts\sender\values.yaml --debug
 
 Set-Location $InitLocation
