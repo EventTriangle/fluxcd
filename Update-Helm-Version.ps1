e2e7ae715b27993aa86da173f4c67b4ad60100a6
Petro Kolosov <kolosovp94@gmail.com>
2026-04-19 10:20:04 +0200

    update helm charts

--- Update-Helm-Version.ps1
+++ Update-Helm-Version.ps1
@@ -1,5 +1,6 @@
 param(
-    [string] $Version
+    [string] $Version,
+    [string] $Tag="latest"
 )
 
 $ErrorActionPreference = "Stop"
@@ -8,11 +9,15 @@ Write-Host "Received version: $Version"
 
 yq -i ".version = `"$Version`"" "charts/auth/Chart.yaml"
 yq -i ".appVersion = `"$Version`"" "charts/auth/Chart.yaml"
+yq -i ".image.tag = `"$Tag`"" "charts/auth/values.yaml"
 
 yq -i ".version = `"$Version`"" "charts/consumer/Chart.yaml"
 yq -i ".appVersion = `"$Version`"" "charts/consumer/Chart.yaml"
+yq -i ".image.tag = `"$Tag`"" "charts/consumer/values.yaml"
 
 yq -i ".version = `"$Version`"" "charts/sender/Chart.yaml"
 yq -i ".appVersion = `"$Version`"" "charts/sender/Chart.yaml"
+yq -i ".image.tag = `"$Tag`"" "charts/sender/values.yaml"
 
+Write-Host "Tag updated to $Tag." -ForegroundColor Green
 Write-Host "Version updated to $Version." -ForegroundColor Green
