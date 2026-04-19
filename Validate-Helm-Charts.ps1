2977f5275de30dcfa297743a3edc331a51ec244a
Petro Kolosov <kolosovp94@gmail.com>
2026-04-19 16:45:48 +0200

    update helm charts

---
+++ Validate-Helm-Charts.ps1
@@ -0,0 +1,11 @@
+$ErrorActionPreference = "Stop"
+
+$InitLocation = Get-Location
+
+Set-Location $PSScriptRoot
+
+helm template auth-app .\charts\auth\ --values .\charts\auth\values.yaml --debug
+helm template consumer-app .\charts\consumer\ --values .\charts\consumer\values.yaml --debug
+helm template sender-app .\charts\sender\ --values .\charts\consumer\values.yaml --debug
+
+Set-Location $InitLocation
